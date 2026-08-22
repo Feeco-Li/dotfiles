fa1b389d99bb82603d7ed96239aafecdbe2a26b8
Feeco <lifeike67@gmail.com>
2026-08-22 02:16:10 -0600

    Update .config/nvim/lua/plugins/telescope.lua

--- private_dot_config/nvim/lua/plugins/telescope.lua
+++ private_dot_config/nvim/lua/plugins/telescope.lua
@@ -1,8 +1,20 @@
 return {
   "nvim-telescope/telescope.nvim",
-  opts = {
-    defaults = {
+  opts = function(_, opts)
+    local actions = require "telescope.actions"
+    opts.defaults = vim.tbl_deep_extend("force", opts.defaults or {}, {
       multi_icon = "",
-    },
-  },
+      mappings = {
+        i = {
+          ["<Tab>"] = actions.move_selection_worse,
+          ["<S-Tab>"] = actions.move_selection_better,
+        },
+        n = {
+          ["<Tab>"] = actions.move_selection_worse,
+          ["<S-Tab>"] = actions.move_selection_better,
+        },
+      },
+    })
+    return opts
+  end,
 }

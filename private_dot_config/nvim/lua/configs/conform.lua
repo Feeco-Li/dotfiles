007d6b2e3ef401d534adab0596cad1a1e6fb0f70
Feeco <lifeike67@gmail.com>
2026-09-08 18:03:34 -0600

    Update .config/gobang/config.toml Update .config/nvim/lazy-lock.json Update .config/nvim/lua/configs/conform.lua

--- private_dot_config/nvim/lua/configs/conform.lua
+++ private_dot_config/nvim/lua/configs/conform.lua
@@ -12,9 +12,6 @@ require("conform").setup {
     prettier = {
       prepend_args = { "--print-width", "200" },
     },
-    rustfmt = {
-      prepend_args = { "--config", "max_width=200,edition=2021,group_imports=StdExternalCrate" },
-    },
   },
   format_on_save = {
     timeout_ms = 5000, -- increase timeout to 5 seconds
